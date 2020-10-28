837262b0a9535edc1c9db5f24e2826c30478c3d1
muyifang1 <muyifang1@163.com>
2020-10-28 21:11:24 +0800

    homework about the HttpClientDemo

---
+++ Week_02/httpclientdemo/src/main/java/http/client/demo/HttpClientDemo.java
@@ -0,0 +1,31 @@
+package http.client.demo;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+
+/**
+ * HttpClient Demo 访问 http://localhost:8801/
+ *
+ * @author YangQi
+ */
+public class HttpClientDemo {
+    public static void main(String[] args) throws Exception {
+
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+        HttpGet httpGet = new HttpGet("http://localhost:8801/");
+        HttpResponse response = httpClient.execute(httpGet);
+
+        if (response.getStatusLine().getStatusCode() == 200) {
+            HttpEntity resEntity = response.getEntity();
+            String message = EntityUtils.toString(resEntity, "utf-8");
+            System.out.println(message);
+        } else {
+            System.out.println("请求失败");
+        }
+    }
+
+}
